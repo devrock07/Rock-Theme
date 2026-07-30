c6ef7db7c2285b657c06ef84d3687d237e63c2d3
DevRock <devrock07@users.noreply.github.com>
2026-07-30 15:34:53 +0530

    fix: retheme SFTP banner

--- resources/scripts/components/server/settings/SettingsContainer.tsx
+++ resources/scripts/components/server/settings/SettingsContainer.tsx
@@ -43,7 +43,7 @@ export default () => {
                             </div>
                             <div css={tw`mt-6 flex items-center`}>
                                 <div css={tw`flex-1`}>
-                                    <div css={tw`border-l-4 border-cyan-500 p-3`}>
+                                    <div css={tw`border-l-4 border-red-500 p-3`}>
                                         <p css={tw`text-xs text-neutral-200`}>
                                             Your SFTP password is the same as the password you use to access this panel.
                                         </p>
@@ -51,7 +51,7 @@ export default () => {
                                 </div>
                                 <div css={tw`ml-4`}>
                                     <a href={`sftp://${username}.${id}@${ip(sftp.ip)}:${sftp.port}`}>
-                                        <Button.Text variant={Button.Variants.Secondary}>Launch SFTP</Button.Text>
+                                        <Button.Danger variant={Button.Variants.Secondary}>Launch SFTP</Button.Danger>
                                     </a>
                                 </div>
                             </div>
